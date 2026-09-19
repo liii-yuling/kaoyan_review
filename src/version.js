8d02d0857d24a7ead1553c8fd46b87cc0d854cfd
liii-yuling <liii-yuling@users.noreply.github.com>
2026-09-19 08:41:30 +0800

    更新 2026-09-19 00:41:30

--- src/version.js
+++ src/version.js
@@ -2,6 +2,6 @@
 (function (global) {
   'use strict';
   var KY = (global.KY = global.KY || {});
-  KY.BUILD = '20260919-084003';
-  KY.BUILD_TIME = '2026-09-19 08:40:03';
+  KY.BUILD = '20260919-084130';
+  KY.BUILD_TIME = '2026-09-19 08:41:30';
 })(window);
